e80aafe6ce65914761cc35a052709beafd4c26bf
Michel Graciano <michel.graciano@gmail.com>
2013-04-08 23:36:25 -0300

    Corrigido instalação do processor e implementado CC super simples

--- annotation-processors/ap-demo-processors/src/main/java/tdc2013/link/processors/DocumentationProcessor.java
+++ annotation-processors/ap-demo-processors/src/main/java/tdc2013/link/processors/DocumentationProcessor.java
@@ -30,21 +30,50 @@
  */
 package tdc2013.link.processors;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Completion;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 
 @SupportedAnnotationTypes({"tdc2013.link.Documentation"})
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class DocumentationProcessor extends AbstractProcessor {
 
+    private static String[] urls = new String[]{
+        "http://wiki.netbeans.org/",
+        "http://www.wikipedia.org/",
+        "http://wiki.suaempresa.com.br/"};
+
     @Override
     public boolean process(final Set<? extends TypeElement> annotations,
             final RoundEnvironment roundEnv) {
         return false;
+    }
+
+    @Override
+    public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
+        final Set<Completion> c = new LinkedHashSet<>(3);
+        for (final String url : urls) {
+            c.add(new Completion() {
+                @Override
+                public String getValue() {
+                    return '\"' + url + '\"';
+                }
+
+                @Override
+                public String getMessage() {
+                    return "URL para site";
+                }
+            });
+        }
+        return c;
     }
 }
