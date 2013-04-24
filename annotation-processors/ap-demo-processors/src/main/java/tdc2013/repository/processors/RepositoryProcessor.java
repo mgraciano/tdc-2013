b75056e57acf6b26bc11b25e985672478c38b0fb
Michel Graciano <michel.graciano@gmail.com>
2013-04-24 20:54:26 -0300

    Alterado template para gerar classes com @javax.annotation.Generated

--- annotation-processors/ap-demo-processors/src/main/java/tdc2013/repository/processors/RepositoryProcessor.java
+++ annotation-processors/ap-demo-processors/src/main/java/tdc2013/repository/processors/RepositoryProcessor.java
@@ -32,6 +32,8 @@ package tdc2013.repository.processors;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -73,51 +75,53 @@ public class RepositoryProcessor extends AbstractProcessor {
             try {
                 JavaFileObject jfo = processingEnv.getFiler().createSourceFile(
                         classElement.getQualifiedName() + "Impl");
-
-                BufferedWriter bw = new BufferedWriter(jfo.openWriter());
-                bw.append("package ");
-                bw.append(packageElement.getQualifiedName());
-                bw.append(";");
-                bw.newLine();
-                bw.newLine();
-
-                bw.append("public class " + classElement.getSimpleName() + "Impl implements " + classElement.getQualifiedName() + "{");
-
-                bw.newLine();
-                bw.newLine();
-                for (Element _element : classElement.getEnclosedElements()) {
-                    if (_element.getKind() == ElementKind.METHOD) {
-                        ExecutableElement method = ExecutableElement.class.cast(_element);
-                        bw.append("public " + method.getReturnType());
-                        bw.append(" " + method.getSimpleName());
-                        bw.append("(");
-
-                        for (int i = 0; i < method.getParameters().size(); i++) {
-                            VariableElement typeParameterElement = method.getParameters().get(i);
-
-                            bw.append(" " + typeParameterElement.asType());
-                            bw.append(" " + typeParameterElement.getSimpleName());
-
-                            if (i + 1 < method.getParameters().size()) {
-                                bw.append(",");
-                            }
-                        }
-
-
-                        bw.append("){");
-                        bw.append("System.out.println(\"" + method.getSimpleName() + "\");");
-                        bw.newLine();
-                        bw.append("return null;");
-                        bw.newLine();
-                        bw.append("}");
-                    }
+                try (BufferedWriter bw = new BufferedWriter(jfo.openWriter())) {
+                    bw.append("package ");
+                    bw.append(packageElement.getQualifiedName());
+                    bw.append(";");
                     bw.newLine();
+                    bw.newLine();
+
+                    bw.append("@javax.annotation.Generated(value = \""
+                            + getClass().getPackage().getName() + "\", date = \""
+                            + new SimpleDateFormat().format(new Date()) + "\")");
+                    bw.newLine();
+                    bw.append("public class " + classElement.getSimpleName() + "Impl implements " + classElement.getQualifiedName() + "{");
+
+                    bw.newLine();
+                    bw.newLine();
+                    for (Element _element : classElement.getEnclosedElements()) {
+                        if (_element.getKind() == ElementKind.METHOD) {
+                            ExecutableElement method = ExecutableElement.class.cast(_element);
+                            bw.append("public " + method.getReturnType());
+                            bw.append(" " + method.getSimpleName());
+                            bw.append("(");
+
+                            for (int i = 0; i < method.getParameters().size(); i++) {
+                                VariableElement typeParameterElement = method.getParameters().get(i);
+
+                                bw.append(" " + typeParameterElement.asType());
+                                bw.append(" " + typeParameterElement.getSimpleName());
+
+                                if (i + 1 < method.getParameters().size()) {
+                                    bw.append(",");
+                                }
+                            }
+
+
+                            bw.append("){");
+                            bw.append("System.out.println(\"" + method.getSimpleName() + "\");");
+                            bw.newLine();
+                            bw.append("return null;");
+                            bw.newLine();
+                            bw.append("}");
+                        }
+                        bw.newLine();
+                    }
+
+                    bw.newLine();
+                    bw.append("}");
                 }
-
-                bw.newLine();
-                bw.append("}");
-
-                bw.close();
             } catch (IOException ex) {
                 Logger.getLogger(RepositoryProcessor.class.getName()).log(Level.SEVERE, null, ex);
             }
