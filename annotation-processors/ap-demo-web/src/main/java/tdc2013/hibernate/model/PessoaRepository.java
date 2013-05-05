7d7e9abe4137ba0abfa911558925740f4c488f9f
Michel Graciano <michel.graciano@gmail.com>
2013-05-05 16:35:04 -0300

    Removed unnecessary comment

--- annotation-processors/ap-demo-web/src/main/java/tdc2013/hibernate/model/PessoaRepository.java
+++ annotation-processors/ap-demo-web/src/main/java/tdc2013/hibernate/model/PessoaRepository.java
@@ -28,10 +28,6 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package tdc2013.hibernate.model;
 
 import tdc2013.repository.Repository;
