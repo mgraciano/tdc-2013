d8ccf1fdd82882833a51d8a61317e721bef690f8
Michel Graciano <michel.graciano@gmail.com>
2013-05-14 21:52:20 -0300

    More code clean up

--- annotation-processors/ap-demo-processors/src/main/java/tdc2013/hibernate/processors/TypdeDefsVisitor.java
+++ annotation-processors/ap-demo-processors/src/main/java/tdc2013/hibernate/processors/TypdeDefsVisitor.java
@@ -41,6 +41,10 @@ import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.PackageElement;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 
 public class TypdeDefsVisitor extends AbstractElementVisitor<Set<String>, Void> {
 
@@ -68,5 +72,88 @@ public class TypdeDefsVisitor extends AbstractElementVisitor<Set<String>, Void> 
 
         Logger.getGlobal().log(Level.WARNING, "Names {0}...", info.toString());
         return info.getTypesNames();
+    }
+}
+
+class TypeDefNameVisitor extends AbstractTypeDefVisitor<Void, TypdeDefsInfo> {
+
+    @Override
+    public Void visitAnnotation(AnnotationMirror a, TypdeDefsInfo info) {
+        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : a.getElementValues().entrySet()) {
+            final ExecutableElement ee = entry.getKey();
+            if (ee.getSimpleName().contentEquals("name")) {
+                info.getTypesNames().add(entry.getValue().getValue().toString());
+            }
+        }
+        return null;
+    }
+
+}
+
+class TypeDefEnumClassVisitor extends AbstractTypeDefVisitor<Void, TypdeDefsInfo> {
+
+    private final ProcessingEnvironment processingEnv;
+
+    TypeDefEnumClassVisitor(ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+    }
+
+    @Override
+    public Void visitAnnotation(final AnnotationMirror a, final TypdeDefsInfo info) {
+        visitTypeDef(a, info);
+        visitTypeDefParameterAnn(a, info);
+        return null;
+    }
+
+    private void visitTypeDef(final AnnotationMirror a, final TypdeDefsInfo info) {
+        if (!isSameType(a.getAnnotationType(), "org.hibernate.annotations.TypeDef")) {
+            return;
+        }
+
+        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : a.getElementValues().entrySet()) {
+            final ExecutableElement ee = entry.getKey();
+            if (ee.getSimpleName().contentEquals("typeClass") && !entry.getValue().getValue().toString().equals(
+                    "tdc2013.hibernate.EnumValueUserType")) {
+                return;
+            }
+        }
+
+        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : a.getElementValues().entrySet()) {
+            final ExecutableElement ee = entry.getKey();
+            if (ee.getSimpleName().contentEquals("parameters")) {
+                entry.getValue().accept(this, info);
+            }
+        }
+    }
+
+    private void visitTypeDefParameterAnn(final AnnotationMirror a, final TypdeDefsInfo info) {
+        if (!isSameType(a.getAnnotationType(), "org.hibernate.annotations.Parameter")) {
+            return;
+        }
+
+        for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : a.getElementValues().entrySet()) {
+            final ExecutableElement ee = entry.getKey();
+            if (ee.getSimpleName().contentEquals("value")) {
+                final String enumClassName = entry.getValue().getValue().toString();
+                if (!isSubtype(enumClassName, "tdc2013.hibernate.EnumValue")) {
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                            enumClassName + " deve implementar tdc2013.hibernate.EnumValue.", info.getRootElement(), a,
+                            entry.getValue());
+                }
+            }
+        }
+    }
+
+    private boolean isSubtype(final String t1, final String t2) {
+        final Elements elements = processingEnv.getElementUtils();
+        final Types types = processingEnv.getTypeUtils();
+        return types.isSubtype(types.erasure(elements.getTypeElement(t1).asType()), types.erasure(elements.
+                getTypeElement(t2).asType()));
+    }
+
+    private boolean isSameType(final TypeMirror at, final String type) {
+        final Elements elements = processingEnv.getElementUtils();
+        final Types types = processingEnv.getTypeUtils();
+        return types.isSameType(types.erasure(at), types.erasure(elements.getTypeElement(type).asType()));
     }
 }
